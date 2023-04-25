2ebb856a2fdc387f67170e39666201d23ff2c2f9
Aldehir Rojas <hello@aldehir.com>
2023-04-24 20:38:36 -0500

    Add radius of 3 options

--- generate.py
+++ generate.py
@@ -88,7 +88,7 @@ def create_texture(directory, prefix, surface):
 width = 64
 height = 64
 
-radii = [0.5, 1.0, 2.0, 2.5, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0]
+radii = [0.5, 1.0, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0]
 thicknesses = [1.0, 2.0, 3.0, 4.0, 5.0]
 
 for r in radii:
